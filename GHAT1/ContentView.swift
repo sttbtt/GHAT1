75d5dae3c51f242663b1d6e0782289f319170a26
Scott Bennett <stt.btt@gmail.com>
2023-04-18 11:47:26 -0400

    Initial Commit

---
+++ GHAT1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GHAT1
+//
+//  Created by Scott Bennett on 4/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GHAT1/GHAT1App.swift
@@ -0,0 +1,17 @@
+//
+//  GHAT1App.swift
+//  GHAT1
+//
+//  Created by Scott Bennett on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct GHAT1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
