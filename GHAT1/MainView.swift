d002e7e6e9f9e528e108f1709e5ca8033bcecf4b
Scott Bennett <stt.btt@gmail.com>
2023-04-29 16:40:02 -0400

    Test lint

--- GHAT1/MainView.swift
+++ GHAT1/MainView.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 struct MainView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+var body: some View {
+VStack {
+Image(systemName: "globe")
+    .imageScale(.large)
+    .foregroundColor(.accentColor)
+Text("Hello, world!")
+}
+.padding()
+}
 }
 
 struct MainView_Previews: PreviewProvider {
